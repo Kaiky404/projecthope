b42085e09ce4548d5fe778560bbafd41a984e60d
Kaiky <2004kfs@gmail.com>
2025-03-19 02:13:10 -0300

    funcionalidade de consulta implementada, falta os botões

--- scriptsistemas/consulta.js
+++ scriptsistemas/consulta.js
@@ -16,40 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
                     <p id="modal-species"><strong class="label-modal">Espécie:</strong>${cadastro.animal.especie}</p>
                     <p id="modal-breed"><strong class="label-modal">Raça:</strong>${cadastro.animal.raca}</p>
                     <p id="modal-gender"><strong class="label-modal">Sexo:</strong>${cadastro.animal.sexo}</p>
-                    <p id="modal-age"><strong class="label-modal">Idade:</strong> ${cadastro.animal.idade}</p>
                     <p id="modal-size"><strong class="label-modal">Porte:</strong> ${cadastro.animal.porte}</p>
-                    <p id="modal-fur"><strong class="label-modal">Pelagem:</strong> ${cadastro.animal.pelagem}</p>
                 </div>
                 <h2 class="title-modal">Informações Basicas do Tutor</h2>
                 <div class="row">
                     <p id="modal-tutor"><strong class="label-modal">Nome:</strong>${cadastro.guardiao.tutor}</p>
-                    <p id="modal-cpf"><strong class="label-modal">CPF:</strong>${cadastro.guardiao.cpf}</p>
                     <p id="modal-endereco"><strong class="label-modal">Endereço:</strong>${cadastro.guardiao.endereco}</p>
+                    <p id="modal-telefone"><strong class="label-modal">Endereço:</strong>${cadastro.guardiao.telefone}</p>
                 </div>
             </div>
-            <h2 class="title-modal">Histórico Médico</h2>
+            <h2 class="title-modal">Sobre a consulta</h2>
             <div class="row">
-                <p><strong class="label-modal">Doença Anteriores:</strong><input type="textarea" id="modal-doencaAnt" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.doencaAnt || ''}" /></p>
-                <p><strong class="label-modal">Tratamentos Realizados:</strong><input type="textarea" id="modal-tratamentoAnt" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.tratamentoAnt || ''}" /></p>
-                <p><strong class="label-modal">Histórico de Vacinação:</strong><input type="textarea" id="modal-histVacina" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.histVacina || ''}" /></p>
-            </div>
-            <h2 class="title-modal">Outros</h2>
-            <div class="row">
-                <p><strong class="label-modal">sintomas atuais e queixas apresentadas:</strong><input type="text" id="modal-anamnese" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.anamnese || ''}" /></p>
-                <p><strong class="label-modal">Exame Físico:</strong><input type="textarea" id="modal-examFisico" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.examFisico || ''}" /></p>
-                <p><strong class="label-modal">Diagnóstico:</strong><input type="textarea" id="modal-diagnostico" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.diagnostico || ''}" /></p>    
-            </div>
-            <h2 class="title-modal">Tratamento</h2>
-            <div class="row">
-                <p><strong class="label-modal">Prescrição de Medicamentos:</strong><input type="textarea" id="modal-prescMedicamentos" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.prescMedicamentos || ''}" /></p>
-                <p><strong class="label-modal">Procedimentos Realizados:</strong><input type="textarea" id="modal-procedRealizado" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.procedRealizado || ''}" /></p>
-                <p><strong class="label-modal">Orientações ao Tutor:</strong><input type="textarea" id="modal-orientacoes" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.orientacoes || ''}" /></p>
-                <p><strong class="label-modal">Evolução Clínica:</strong><input type="textarea" id="modal-evoClinica" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.evoClinica || ''}" /></p>
+                <p><strong class="label-modal">Data:</strong><input type="date" id="modal-data" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.data || ''}" /></p>
+                <p><strong class="label-modal">Horário:</strong><input type="time" id="modal-horario" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.horario || ''}" /></p>
+                <p><strong class="label-modal">Tipo da Consulta:</strong><input type="textarea" id="modal-tipoConsulta" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.tipoConsulta || ''}" /></p>
             </div>
             <h2 class="title-modal">Profissional</h2>
             <div class="row">
                 <p><strong class="label-modal">Nome do Profissional:</strong><input type="text" id="modal-nomeProfissional" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.nomeProfissional || ''}" /></p>
-                <p><strong class="label-modal">RA do Profissional:</strong><input type="text" id="modal-raProfissional" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.raProfissional || ''}" /></p>
+                <p><strong class="label-modal">RA do Profissional (crmv):</strong><input type="text" id="modal-raProfissional" value="${JSON.parse(localStorage.getItem(`prontuario-${cadastro.animal.paciente}`))?.raProfissional || ''}" /></p>
             </div>
 
             <div class="buttons">
@@ -75,16 +60,9 @@ document.addEventListener("DOMContentLoaded", function () {
     function salvarProntuario(index) {
         const cadastro = listaCadastros[index];
         const prontuario = {
-            doencaAnt: document.getElementById("modal-doencaAnt").value,
-            tratamentoAnt: document.getElementById("modal-tratamentoAnt").value,
-            histVacina: document.getElementById("modal-histVacina").value,
-            anamnese: document.getElementById("modal-anamnese").value,
-            examFisico: document.getElementById("modal-examFisico").value,
-            diagnostico: document.getElementById("modal-diagnostico").value,
-            prescMedicamentos: document.getElementById("modal-prescMedicamentos").value,
-            procedRealizado: document.getElementById("modal-procedRealizado").value,
-            orientacoes: document.getElementById("modal-orientacoes").value,
-            evoClinica: document.getElementById("modal-evoClinica").value,
+            data: document.getElementById("modal-data").value,
+            horario: document.getElementById("modal-horario").value,
+            tipoConsulta: document.getElementById("modal-tipoConsulta").value,
             nomeProfissional: document.getElementById("modal-nomeProfissional").value,
             raProfissional: document.getElementById("modal-raProfissional").value
         };
